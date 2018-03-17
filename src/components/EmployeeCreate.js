125428bdfb05db62aab69e45d9e8d956bc76fd73
handzel <handzelewicz.dev@gmail.com>
2018-03-17 15:06:42 +0100

    EmployeeCreate component boilerpalte/template

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
+
 
 class EmployeeCreate extends Component {
   render() { 
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Chris"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="XXX-XXX-XXX"
+          />
+        </CardSection>
+
+        <CardSection>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+
+        
+      </Card>
     );
   }
 }
