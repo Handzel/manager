93bc662e8c682839833c05f4fa7f328b1745cf18
handzel <handzelewicz.dev@gmail.com>
2018-03-17 16:10:28 +0100

    Hooking up Actions on component via connect() with proper reducer

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 
@@ -10,6 +12,12 @@ class EmployeeCreate extends Component {
           <Input
             label="Name"
             placeholder="Chris"
+            value={this.props.name}
+            // this is a call to Action Creator (EmployeeActions) with its specified input signature
+            // thanks to its design we only call ONE single Action Creator
+            // I use ({ ... , value }) -> ES6 syntax shorthand if key=value like here value: value, 
+            // use just value
+            onChangeText={(value) => this.props.employeeUpdate({ prop: 'name', value })}
           />
         </CardSection>
 
@@ -17,6 +25,12 @@ class EmployeeCreate extends Component {
           <Input
             label="Phone"
             placeholder="XXX-XXX-XXX"
+            value={this.props.phone}
+            // this is a call to Action Creator (EmployeeActions) with its specified input signature
+            // thanks to its design we only call ONE single Action Creator
+            // I use ({ ... , value }) -> ES6 syntax shorthand if key=value like here value: value, 
+            // use just value
+            onChangeText={(value) => this.props.employeeUpdate({ prop: 'phone', value })}
           />
         </CardSection>
 
@@ -35,4 +49,11 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+  // REMEMBER: state.employeeForm -> name of a reducer from reducer/index.js
+  const { name, phone, shift } = state.employeeForm; 
+  // ES6 destructurizing
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
