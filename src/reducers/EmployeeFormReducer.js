d15b00d054661dc2961685b268f48d33eb44d9b5
handzel <handzelewicz.dev@gmail.com>
2018-03-17 15:28:41 +0100

    Reducer for EMPLOYEE_UPDATE action in EmployeeCreate Component

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -2,10 +2,18 @@ import {
   EMPLOYEE_UPDATE
 } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMPLOYEE_UPDATE: 
+      // [] is not an array its key interpolation, they will be defined at runtime.
+      // Its a ES6 syntax trick for generic, dynamic use of props ie. name, phone, shift, etc 
+      return { ...state, [action.payload.prop]: action.payload.value };
     default: 
       return state;
   }
