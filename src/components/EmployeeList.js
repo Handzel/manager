b426f676dc296576053996b6a3da62a2ae91cf98
handzel <handzelewicz.dev@gmail.com>
2018-03-25 22:18:39 +0200

    Transforming objects to Array using lodash

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ListView, View, Text } from 'react-native';
@@ -26,7 +27,8 @@ class EmployeeList extends Component {
     const ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 != r2
     });
-
+    // cloneWithRows works with arrays not Objects -> Object is being returned 
+    // from firebase by default. Solution -> lodash
     this.dataSource = ds.cloneWithRows(employees);
   }
 
@@ -43,6 +45,23 @@ class EmployeeList extends Component {
   }
 }
 
+// will need to solve array vs object problem for ds -> lodash
+const mapStateToProps = state => {
+  // state.employees is an object with many key:value pairs
+  // _.map -> for each key:value pair in state.employees run 
+  // (val, uid) => {} function
+  // uid -> key
+  // val -> userModel has name, phone, shift property
+  // return { ...val, uid } -> then create new object, push in all the values 
+  // (...val) from userModel and add on top uid
+  // output -> name: Name, phone: Phone, shift: Shift, id: ididid
+  // map turns that into array and array is signed to employees const
 
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid };
+  });
 
-export default connect(null, { employeesFetch })(EmployeeList);
+  return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
