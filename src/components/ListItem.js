5a9b31f1b58d97f596e9ada23a06e66de7f15ae7
handzel <handzelewicz.dev@gmail.com>
2018-03-26 20:53:14 +0200

    ListItem component prep. Link from list to create view

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableWithoutFeedback } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import { CardSection } from './common';
 
 class ListItem extends Component {
+  onRowPress() {
+    // Nice & Easy
+    // passing inside function employeeCreate context of clicked prop
+    Actions.employeeCreate({ employee: this.props.employee });
+  }
+  
   render() {
     const { name } = this.props.employee;
 
     return (
-      <CardSection>
-        <View style={styles.titleStyle}>
-          <Text>{name}</Text>
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+        <View> 
+         <CardSection>
+            <Text>{name}</Text>
+          </CardSection>
         </View>
-      </CardSection>
+      </TouchableWithoutFeedback>
     );
   }
 }
 
-const styles = {
-  titleStyle: {
-    fontSize: 18,
-    paddingLeft: 15
-  }
-};
+// const styles = {
+//   titleStyle: {
+//     fontSize: 18,
+//     paddingLeft: 15
+//   }
+// };
 
 export default ListItem;
